546a4de0d64693d3604b9c157e450e64909df774
Christoffer Lindberg <cottelindberg@hotmail.com>
2017-05-14 20:38:46 +0200

    Removed comments

--- js/snake.js
+++ js/snake.js
@@ -1,47 +1,32 @@
-/**
- * Snake class, x and y is starting position.
- * 
- * @param {type} x Horizontal starting positon for snake.
- * @param {type} y Verical starting position for snake.
- * @param {type} worldW The Width of the world.
- * @param {type} worldH The Height of the world.
- * @returns {Snake}
- */
 function Snake(x, y, worldW, worldH) {
 
     var speed = boxSize;
     var worldWidth = worldW;
     var worldHeight = worldH;
-    // last will be the "head" of the snake.
-    var last = {x: x, y: y};
-    // The body of snake, at start put in "head".
+    var headOfSnake = {x: x, y: y};
     var body = new Array();
-    body.push(last);
+    body.push(headOfSnake);
 
     var isAlive = true;
     var dir = {38: false, 40: false, 37: false, 39: false};
     var keys = {UP: 38, DOWN: 40, LEFT: 37, RIGHT: 39};
     
-    /**
-     * Handles moving, and does checks to see if snake is still alive after move.
-     */
     this.move = function () {
-
         if (dir[keys.UP]) {
-            body.unshift({x: last.x, y: last.y - speed});
-            last = body[0];
+            body.unshift({x: headOfSnake.x, y: headOfSnake.y - speed});
+            headOfSnake = body[0];
 
         } else if (dir[keys.DOWN]) {
-            body.unshift({x: last.x, y: last.y + speed});
-            last = body[0];
+            body.unshift({x: headOfSnake.x, y: headOfSnake.y + speed});
+            headOfSnake = body[0];
 
         } else if (dir[keys.LEFT]) {
-            body.unshift({x: last.x - speed, y: last.y});
-            last = body[0];
+            body.unshift({x: headOfSnake.x - speed, y: headOfSnake.y});
+            headOfSnake = body[0];
 
         } else if (dir[keys.RIGHT]) {
-            body.unshift({x: last.x + speed, y: last.y});
-            last = body[0];
+            body.unshift({x: headOfSnake.x + speed, y: headOfSnake.y});
+            headOfSnake = body[0];
         }
         // Removes tail.
         if (body.length > 1) {
@@ -49,8 +34,8 @@ function Snake(x, y, worldW, worldH) {
         }
 
         // If snake is out of bounds. kill it.
-        if (last.x < 0 || (last.x + boxSize) > worldWidth || last.y < 0 || 
-                (last.y + boxSize) > worldHeight) {
+        if (headOfSnake.x < 0 || (headOfSnake.x + boxSize) > worldWidth || headOfSnake.y < 0 || 
+                (headOfSnake.y + boxSize) > worldHeight) {
             isAlive = false;
         // If snake collided with body... kill it.    
         } else if (this.ranIntoItself()) {
@@ -58,11 +43,6 @@ function Snake(x, y, worldW, worldH) {
         }
     };
     
-    /**
-     * Method for handling input from user.
-     * @param {type} e Input event
-     * @returns {undefined|Snake.dir|Boolean}
-     */
     this.doOnKeyDown = function (e) {
         var key = e.keyCode;
         
@@ -99,39 +79,24 @@ function Snake(x, y, worldW, worldH) {
             return dir[keys.LEFT] = true;
         }
     };
-    /**
-     * Expands size of snake with value
-     * @param {int} value
-     */
+    
     this.grow = function (value) {
         for(var i = 0; i < value; i++)
             body.push(body[body.length - 1]);
     };
     
-    /**
-     * A getter for isAlive.
-     * returns true if so, false if dead.
-     * @returns {boolean}
-     */
     this.isAlive = function () {
         return isAlive;
     };
-    /**
-     * Getter for "head" of snake. contains x and y position.
-     * @returns {object} head of snake
-     */
+    
     this.head = function () {
-        return last;
+        return headOfSnake;
     };
     
-    /**
-     * Checks if the snake head has collided with body.
-     * @returns {Boolean}
-     */
     this.ranIntoItself = function () {
         if (body.length > 2) {
             for (var i = 2; i < body.length; i++) {
-                if (last.x === body[i].x && last.y === body[i].y) {
+                if (headOfSnake.x === body[i].x && headOfSnake.y === body[i].y) {
                     return true;
                 }
             }
@@ -139,28 +104,15 @@ function Snake(x, y, worldW, worldH) {
         return false;
     };
 
-    /**
-     * Draws snake on canvas.
-     * @param {context} ctx
-     */
     this.draw = function (ctx) {
         ctx.save();
         ctx.fillStyle = '#3998db';
         for (var i = 0, size = body.length; i < size; i++) {
-      
-                ctx.fillRect(body[i].x, body[i].y, boxSize, boxSize);
-            
+                ctx.fillRect(body[i].x, body[i].y, boxSize, boxSize);         
         }
         ctx.restore();
     };
     
-    /**
-     * Returns true if given position exists in snake body. Is used when 
-     *     checking if fruit can spawn.
-     * @param {int} otherX
-     * @param {int} otherY
-     * @returns {Boolean}
-     */
     this.isAt = function (otherX, otherY) {
         size = body.length;
         for (var i = 0; i < size; i++) {
@@ -171,4 +123,3 @@ function Snake(x, y, worldW, worldH) {
         return false;
     };
 }
-
